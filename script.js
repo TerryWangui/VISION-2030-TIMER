029eef0b57024ba1fa5c66c5779bcf29ff043d1f
TerryWangui <kuiterry100@gmail.com>
2023-10-09 02:20:40 -0900

    completed project

---
+++ script.js
@@ -0,0 +1,55 @@
+const countItems=document.querySelectorAll(".timer span");
+var year=2030;
+var month=1;
+var day=1;
+var hours=12;
+var minutes=0;
+var seconds=0;
+const futureDate= new Date(year,month,day,hours,minutes,seconds);
+var endTime=futureDate.getTime();
+
+function getremainingTime(){
+    var currentTime=new Date().getTime();
+    var remainingTime=endTime-currentTime; 
+    /*
+    1 sec=1000ms
+    1 min=60000ms-60*1000
+    1 hr=3600000ms-60*60*1000
+    1 day=86400000ms-24*60*60*1000
+    1 month=30*24*60*60*1000
+    1 year=12*30*24*60*60*1000
+    */
+   const oneYear=12*30*24*60*60*1000;
+   const oneMonth=30*24*60*60*1000;
+   const oneDay=24*60*60*1000;
+   const oneHour=60*60*1000;
+   const oneMinute=60*1000;
+   const oneSecond=1000;
+
+   var rYears=Math.floor(remainingTime/oneYear);
+   var rMonths=Math.floor((remainingTime%oneYear)/oneMonth);
+   var rDays=Math.floor((remainingTime%oneMonth)/oneDay);
+   var rHours=Math.floor((remainingTime%oneDay)/oneHour);
+   var rMinutes=Math.floor((remainingTime%oneHour)/oneMinute);
+   var rSeconds=Math.floor((remainingTime%oneMinute)/oneSecond);
+
+   const rValues=[rYears,rMonths,rDays,rHours,rMinutes,rSeconds];
+
+   function formatDigits(item){
+    if(item<10){
+        return`0${item}`;
+        
+    }
+    return item;
+   }
+   countItems.forEach((item,index)=>{
+    item.innerHTML= formatDigits(rValues[index]);
+   });
+
+   if(remainingTime<0){
+    clearInterval(countDown);
+   }
+
+}
+let countDown=setInterval(getremainingTime,1000);
+getremainingTime();
